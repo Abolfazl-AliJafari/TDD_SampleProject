d0be41b8380c0076364cb0a5fe0fc3d16ac42947
Abolfazl-Alijafari-Taheri <abolfazlalijafari59@gmail.com>
2024-09-17 17:21:23 +0330

    Add gets tests

--- TaskManagment.Test/TaskServiceTest.cs
+++ TaskManagment.Test/TaskServiceTest.cs
@@ -47,5 +47,37 @@ namespace TaskManagment.Test
             var task = taskService.GetTaskById(id);
             Assert.Null(task); 
         }
+
+        [Fact]
+        public void getTasks_should_return_all_tasks()
+        {
+            // Arrange
+            var taskService = new TaskService();
+            taskService.AddTask("TDD");
+            taskService.AddTask("BDD");
+
+            // Act
+            var tasks = taskService.GetTasks();
+
+            // Assert
+            Assert.Equal(2, tasks.Count);
+        }
+
+        [Fact]
+        public void gettaskbystatus_filter_tasks_by_status()
+        {
+            // Arrange
+            var taskService = new TaskService();
+            var id1 = taskService.AddTask("TDD");
+            var id2 = taskService.AddTask("BDD");
+            taskService.UpdateTask(id1, "مطالعه C#", TaskStatuses.Done);
+
+            // Act
+            var completedTasks = taskService.GetTaskByStatus(TaskStatuses.Done);
+
+            // Assert
+            Assert.Single(completedTasks);
+            Assert.Equal(TaskStatuses.Done, completedTasks[0].Status);
+        }
     }
 }
