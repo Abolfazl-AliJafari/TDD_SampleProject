9381d2670578f25f0e1ffbb90bab1055481b0b95
Abolfazl-Alijafari-Taheri <abolfazlalijafari59@gmail.com>
2024-09-17 13:52:18 +0330

    Add taskserviceTest

---
+++ TaskManagment.Test/TaskServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagment.Test
+{
+    public class TaskServiceTest
+    {
+        [Fact]
+        public void addtask_to_tasks_list()
+        {
+            var taskService = new TaskService();
+
+        }
+    }
+}
